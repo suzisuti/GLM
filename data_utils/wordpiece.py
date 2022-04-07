566c9fe9c19c237a58dc90979f2695dc622dec16
Suzi Park <41722206+suzisuti@users.noreply.github.com>
2022-04-07 11:23:11 +0900

    Add files via upload
    snunlp/KR-BERT 토크나이저를 사용할 수 있도록 수정

--- data_utils/wordpiece.py
+++ data_utils/wordpiece.py
@@ -34,6 +34,7 @@ PRETRAINED_VOCAB_ARCHIVE_MAP = {
     'bert-base-multilingual-uncased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-uncased-vocab.txt",
     'bert-base-multilingual-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-cased-vocab.txt",
     'bert-base-chinese': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-chinese-vocab.txt",
+    'snunlp/KR-Medium': "/home/suzi/KR-Medium/vocab.txt",
 }
 PRETRAINED_VOCAB_POSITIONAL_EMBEDDINGS_SIZE_MAP = {
     'bert-base-uncased': 512,
@@ -43,6 +44,7 @@ PRETRAINED_VOCAB_POSITIONAL_EMBEDDINGS_SIZE_MAP = {
     'bert-base-multilingual-uncased': 512,
     'bert-base-multilingual-cased': 512,
     'bert-base-chinese': 512,
+    'snunlp/KR-Medium': 512,
 }
 VOCAB_NAME = 'vocab.txt'
 
